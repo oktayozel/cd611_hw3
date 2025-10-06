bb6ad5b212bac78841e89569f20f61fd47e6a760
Oktay Özel <mail.oktayozel@gmail.com>
2025-10-06 16:50:00 -0400

    Fix compile.sh to include encoding option

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-javac -d out puzzles/app/*.java puzzles/core/*.java puzzles/io/*.java puzzles/games/sliding_puzzle_components/*.java \
+javac -encoding UTF-8 -d out puzzles/app/*.java puzzles/core/*.java puzzles/io/*.java puzzles/games/sliding_puzzle_components/*.java \
 && cp puzzles/resources/config.properties out/ 
