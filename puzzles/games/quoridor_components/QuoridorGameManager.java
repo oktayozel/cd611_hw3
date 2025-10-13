3d73a88b4e7051ea0da794b65a0b061c0acb04fe
Oktay Özel <oktayozel@Oktays-MBP-2.cable.rcn.com>
2025-10-13 12:58:11 -0400

    implemented initializePlayers and initializeBoard functions in game manager

--- puzzles/games/quoridor_components/QuoridorGameManager.java
+++ puzzles/games/quoridor_components/QuoridorGameManager.java
@@ -4,6 +4,7 @@ import puzzles.core.LeaderBoard;
 import puzzles.io.Input;
 import puzzles.io.Output;
 import puzzles.core.Settings;
+import puzzles.games.dots_and_boxes_components.DotsAndBoxesUser;
 
 public class QuoridorGameManager extends GameManager {
     
@@ -18,9 +19,40 @@ public class QuoridorGameManager extends GameManager {
         this.input  = new Input();
         this.output = new Output(this.input, "quoridor");
         this.leaderBoard = new LeaderBoard();
-        this.settings = new Settings();
+        this.settings = settings;
     }
 
+     @Override
+    public void initGame(boolean gameFirstOpen) {
+        leaderBoard.loadLeaderBoard();
+
+        initializePlayers(gameFirstOpen);
+        initializeBoard();
+
+    }
+
+    // Method to prompt for player names and create player objects.
+    @Override
+    protected void initializePlayers(boolean gameFirstOpen) {
+
+        if(gameFirstOpen == true){
+            output.printWelcomeMessage();
+            String name1 = input.readStringOrExit("Enter Player 1 name: ");
+            player1 = new DotsAndBoxesUser(name1, "P1");
+            String name2 = input.readStringOrExit("Enter Player 2 name: ");
+            player2 = new DotsAndBoxesUser(name2, "P2");
+        
+        }
+    }
+
+    @Override
+    protected void initializeBoard(){
+        int rows = input.readIntOrExit(String.format("Enter number of rows: "), settings.getMinBoardSize("Quoridor"), settings.getMaxBoardSize("Quoridor"));
+        int cols = input.readIntOrExit(String.format("Enter number of columns: "), settings.getMinBoardSize("Quoridor"), settings.getMaxBoardSize("Quoridor"));
+        this.board = new QuoridorBoard(rows, cols, new QuoridorUser("Player 1"), new QuoridorUser("Player 2"));
+    }
+
+    
 
 
 
